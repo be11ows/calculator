8b6a035d806db563462961c771d9c5bf0a782b61
istvan kosa <istvankosa@istvans-iMac.local>
2022-09-06 12:15:13 -0700

    Add basic structure, style, and functions

---
+++ main.js
@@ -0,0 +1,52 @@
+function add (n1, n2) {
+    return n1 + n2;
+}
+
+function subtract(n1, n2) {
+    return n1 - n2;
+}
+
+function multiply (n1, n2) {
+    return n1 * n2;
+}
+
+function divide(n1, n2) {
+    return n1 / n2;
+}
+
+function operate(n1, op, n2) {
+    let output;
+    
+    switch (op) {
+        case '+':
+            output = add(n1, n2);
+            break;
+        case '-':
+            output = subtract(n1, n2);
+            break;
+        case '*':
+            output = multiply(n1, n2);
+            break;
+        case '/':
+            output = divide(n1, n2);
+            break;
+    }
+    // output should be displayed on the calculator screen
+    console.log(output);
+}
+
+function equals() {
+
+}
+
+function display() {
+    // Create the functions that populate the display when you click the number buttons. 
+    // You should be storing the ‘display value’ in a variable somewhere for use in the next step.
+} 
+
+// Make the calculator work! 
+// You’ll need to store the first number that is input into the calculator when a user presses an operator, 
+// and also save which operation has been chosen 
+// and then operate() on them when the user presses the “=” key.
+
+operate(2, '/', 5);
