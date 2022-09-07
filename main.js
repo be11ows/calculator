38bfe0bd7898dff9ed3d6822cd4fe2ccb7865b64
istvan kosa <istvankosa@istvans-iMac.local>
2022-09-06 17:35:50 -0700

    Add ability to use decimals in operations

--- main.js
+++ main.js
@@ -5,10 +5,15 @@ let n1, op, n2, display;
 
 const nOpButtons = [...document.querySelectorAll('.num, .operator')];
 const equalsBtn = document.querySelector('.equals');
+const decimalBtn = document.querySelector('.decimal');
+decimalBtn.disabled = true;
+decimalBtn.addEventListener('click', showClicked);
 
 for(let button of nOpButtons) {
     button.addEventListener('click', showClicked);
 };
+
+
 
 function showClicked(e) {
     // NUMBER
@@ -16,6 +21,7 @@ function showClicked(e) {
         if (!display && op === undefined) {
             displayVal.innerText = e.target.innerText;
             display = displayVal.innerText;
+            decimalBtn.disabled = false;
         } else if (display && op === undefined) {
             displayVal.innerText += e.target.innerText;
             display = displayVal.innerText;
@@ -23,6 +29,7 @@ function showClicked(e) {
             n2 = e.target.innerText;
             displayVal.innerText += ` ${n2}`;
             display = displayVal.innerText;
+            decimalBtn.disabled = false;
         } else if (n1 && op && n2) {
             n2 += e.target.innerText;
             displayVal.innerText = `${n1} ${op} ${n2}`;
@@ -41,6 +48,7 @@ function showClicked(e) {
             op = e.target.innerText;
             displayVal.innerText += ` ${op} `;
             display = displayVal.innerText;
+            decimalBtn.disabled = true;
         } else if (n1 && op && n2) {
             n1 = operate(n1, op, n2);
             op = e.target.innerText;
@@ -49,6 +57,25 @@ function showClicked(e) {
             display = displayVal.innerText;
         }
     }
+
+    if (e.target.className === 'decimal') {
+        console.log('decimal clicked');
+        console.log(display);
+        if (n1 && op && n2) {
+            n2 += '.';
+            decimalBtn.disabled = true;
+            displayVal.innerText = `${n1} ${op} ${n2} `;
+            display = displayVal.innerText;
+        } else {
+            console.log(display);
+            display += '.';
+            console.log(display);
+            decimalBtn.disabled = true;
+            displayVal.innerText = `${display} `;
+            display = displayVal.innerText;
+        } 
+    }
+
     equalsBtn.addEventListener('click', (e) => {
         if (n1 && op && n2) {
             displayVal.innerText = operate(n1, op, n2)
@@ -67,7 +94,7 @@ function subtract(n1, n2) {
 }
 
 function multiply (n1, n2) {
-    return +n1 * +n2;
+    return +n1 * +n2
 }
 
 function divide(n1, n2) {
@@ -81,27 +108,15 @@ function operate(n1, op, n2) {
     switch (op) {
         case '+':
             output = add(n1, n2);
-            n1 = output;
-            op = undefined;
-            n2 = undefined;
             break;
         case '-':
             output = subtract(n1, n2);
-            n1 = output;
-            op = undefined;
-            n2 = undefined;
             break;
         case '*':
             output = multiply(n1, n2);
-            n1 = output;
-            op = undefined;
-            n2 = undefined;
             break;
         case '/':
             output = divide(n1, n2);
-            n1 = output;
-            op = undefined;
-            n2 = undefined;
             break;
     }
     return output;
